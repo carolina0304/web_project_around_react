25d2f8e8680d97a6f15c13bde27f7276cfbfac2b
ARLENE GOMEZ <garlene122@gmail.com>
2025-07-06 13:25:08 -0600

    Agregar images, blocks

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "../blocks/header.css";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <img
+          className="header__logo"
+          src="../images/Vector.png"
+          alt="Logotipo Around"
+        />
+        <img className="header__line" src="../images/Line.png" alt="Linea" />
+      </header>
+    </>
+  );
+};
+
+export default Header;
